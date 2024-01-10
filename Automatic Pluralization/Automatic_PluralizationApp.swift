a62fd374972b86bc7f753cefc56b01c571681a80
KMessina1 <KMWeb@Mac.com>
2024-01-10 13:33:35 -0500

    Initial Commit

---
+++ Automatic Pluralization/Automatic_PluralizationApp.swift
@@ -0,0 +1,25 @@
+//
+
+/*--------------------------------------------------------------------------------------------------------------------------
+    File: Automatic_PluralizationApp.swift
+  Author: Kevin Messina
+ Created: 1/10/24
+Modified:
+ 
+©2024 Creative App Solutions, LLC. - All Rights Reserved.
+----------------------------------------------------------------------------------------------------------------------------
+NOTES:
+--------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+import SwiftUI
+
+@main
+struct Automatic_PluralizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
